5550adf0c9b14368b0ddcac7c24cd143b2bf6940
Samantha <samanthadbriseno@gmail.com>
2023-11-04 14:59:00 -0600

    Fix the format of storer principal

--- Principal/StorerPrincipal.cs
+++ Principal/StorerPrincipal.cs
@@ -4,8 +4,8 @@ using Microsoft.VisualBasic;
 
 partial class Program
 {
-     public static void StorersPrincipal()
-     {
+    public static void StorersPrincipal()
+    {
         
         bool exit = false;
 
@@ -19,26 +19,26 @@ partial class Program
                 case "1": //  Add equipment
 
                     WriteLine("Adding a new equipment:");
-                        string equipmentid="", controlnumber="";
-                        int opi=0;        
-                        using(bd_storage db = new())
-                        {
-                            while(opi==0)
-                            {   
-                                WriteLine("Provide the equipment ID for the inventory:");
-                                equipmentid = VerifyReadMaxLengthString(15);
-                                IQueryable<Equipment> equipments = db.Equipments.Where(e=> e.EquipmentId == equipmentid);
+                    string equipmentid="", controlnumber="";
+                    int opi=0;        
+                    using(bd_storage db = new())
+                    {
+                        while(opi==0)
+                        {   
+                            WriteLine("Provide the equipment ID for the inventory:");
+                            equipmentid = VerifyReadMaxLengthString(15);
+                            IQueryable<Equipment> equipments = db.Equipments.Where(e=> e.EquipmentId == equipmentid);
                                 
-                                if(equipments is null || !equipments.Any())
-                                {
-                                    opi = 1;
-                                }
-                                else
-                                {
-                                    WriteLine("That equipment id is already in use, try again.");
-                                }
-                            }                
-                        }
+                            if(equipments is null || !equipments.Any())
+                            {
+                                opi = 1;
+                            }
+                            else
+                            {
+                                WriteLine("That equipment id is already in use, try again.");
+                            }
+                        }                
+                    }
 
                     WriteLine();
                     WriteLine("Provide the equipment name:");
@@ -46,25 +46,25 @@ partial class Program
                     WriteLine();
                     short areaid=-1;
                         
-                        int areasCount = ListAreas();
-                        WriteLine("Choose the area of the equipment:");
-                        while(areaid <= 0 || areaid > areasCount )
+                    int areasCount = ListAreas();
+                    WriteLine("Choose the area of the equipment:");
+                    while(areaid <= 0 || areaid > areasCount )
+                    {
+                        try
                         {
-                            try
-                            {
-                                areaid = Convert.ToInt16(VerifyReadMaxLengthString(2));
-                            }
-                            catch (FormatException)
-                            {
-                                WriteLine("That is not a correct option, try again.");
-                                areaid = -1;
-                            }
-                            catch (OverflowException)
-                            {
-                                WriteLine("That is not a correct option, try again.");
-                                areaid = -1;
-                            }
+                            areaid = Convert.ToInt16(VerifyReadMaxLengthString(2));
                         }
+                        catch (FormatException)
+                        {
+                            WriteLine("That is not a correct option, try again.");
+                            areaid = -1;
+                        }
+                        catch (OverflowException)
+                        {
+                            WriteLine("That is not a correct option, try again.");
+                            areaid = -1;
+                        }
+                    }
                     
                     WriteLine();
                     WriteLine("Provide the description of the equipment:");
@@ -137,133 +137,45 @@ partial class Program
                         }
                         break;
 
-                    case "2": // Update equipment info
-                        UpdateEquipment();
-                    break;
-                    case "3": // Equipment List
-                        ViewAllEquipments();
-                    break;
-                    case "4": // Delete equipment
-                        DeleteEquipment();
-                    break;
-                    case "5":  // List Equipment Requests
-                        ListEquipmentsRequests();
-                    break;
-                    case "6": // LIst Equipment Requests only for tomorrow
-                    break;
-                    case "7":
-                    break;
-                    case "8":
-                        DamagedLostReportInit();
-                        
-                    break;
-                    case "9":
-                    break;
-                    case "10":
-                    break;
-                    case "11":
-                    break;
-                    case "12":
-                    break;
-                    case "13":
-                    break;
-                    case "14":
-                        exit = true;
-                    break;
-                    default:
-                    break;
-                }
-            }   
+                case "2": // Update equipment info
+                    UpdateEquipment();
+                break;
+                case "3": // Equipment List
+                    ViewAllEquipments();
+                break;
+                case "4": // Delete equipment
+                    DeleteEquipment();
+                break;
+                case "5":  // List Equipment Requests
+                    ListEquipmentsRequests();
+                break;
+                case "6": // LIst Equipment Requests only for tomorrow
+
+                break;
+                case "7":
+                break;
+                case "8":
+                break;
+                case "9":
+                break;
+                case "10":
+                    DamagedLostReportInit();
+                break;
+                case "11":
+                break;
+                case "12":
+                break;
+                case "13":
+                break;
+                case "14":
+                    exit = true;
+                break;
+                default:
+                break;
+            }
+        }   
         
-        }
-     }  
-                WriteLine();
-
-                opi=0;
-                    using(bd_storage db = new())
-                    {
-                        while(opi==0)
-                        {   
-                            WriteLine("Insert the control Number provided by the equipments's manufacturer:");
-                            controlnumber = VerifyReadMaxLengthString(20);
-                            IQueryable<Equipment> equipments = db.Equipments.Where(e=> e.ControlNumber == controlnumber);
-                            
-                            if(equipments is null || !equipments.Any())
-                            {
-                                opi = 1;
-                            }
-                            else
-                            {
-                                WriteLine("That control number is already in use, try again.");
-                            }
-                        }                
-                    }
-
-                WriteLine();
-                WriteLine("Choose the coordinator in charge:");
-                string[]? coordinators = ListCoordinators();
-                WriteLine();
-                WriteLine("Write the choosen option:");
-                int coordid = TryParseStringaEntero(VerifyReadLengthStringExact(1));
-                string coordinatorid = "";
-                    if(coordinators is not null)
-                    {
-                        coordinatorid = coordinators[coordid -1];
-                    }
-                    var resultAdd = AddEquipment(equipmentid, name, areaid, description, year, statusid, controlnumber, coordinatorid );
-                    if(resultAdd.affected == 1)
-                    {
-                        WriteLine($"The equipment {resultAdd.EquipmentId} was created succesfully");
-                    }
-                    else{
-                        WriteLine("The equipment was not registered.");
-                    }
-
-                    break;
-
-                    case "2": // Update equipment info
-                        UpdateEquipment();
-                    break;
-
-                    case "3": // Equipment List
-                        ViewAllEquipments();
-                    break;
-
-                    case "4": // Delete equipment
-                        DeleteEquipment();
-                    break;
-
-                    case "5":  // List Equipment Requests
-                        ListEquipmentsRequests();
-                    break;
-
-                    case "6": // LIst Equipment Requests only for tomorrow
-                    break;
-
-                    case "7":
-                    break;
-
-                    case "8":
-                        DamagedLostReportInit();                
-                    break;
-
-                    case "9":
-                    break;
-
-                    case "10":
-                    break;
-
-                    case "11":
-                    break;
-
-                    case "12":
-                    break;
-
-                    default:
-                    break;
-                }   
-            
-    }
+    } 
 
     public static string MenuStorer()
     {
