f36e8e60eada7736617973a9f77763305b7a88fb
carolprian <a20300826@ceti.mx>
2023-11-15 18:14:09 -0600

    Started mock-ips

---
+++ LogInUnitTestings/Services/ITablas.cs
@@ -0,0 +1,31 @@
+using AutoGens;
+using Microsoft.EntityFrameworkCore;
+
+namespace Data
+{
+    public interface IAll:IDisposable
+    {
+        DbSet<Academy> Academies {get;}
+        DbSet<Areas> Areas {get;}
+        DbSet<Classroom> Classrooms {get;}
+        DbSet<Coordinator> Coordinators { get; }
+        DbSet<Division> Divisions { get; }
+        DbSet<DyLequipment> DyLequipments { get;  }
+        DbSet<Equipment> Equipments { get; }
+        DbSet<Group> Groups { get; }
+        DbSet<Maintain> Maintain { get;  }
+        DbSet<MaintenanceRegister> MaintenanceRegisters { get; }
+        DbSet<MaintenanceType> MaintenanceTypes { get; }
+        DbSet<Petition> Petitions { get; }
+        DbSet<PetitionDetail> PetitionDetails { get;  }
+        DbSet<Professor> Professors { get; }
+        DbSet<Request> Requests { get;  }
+        DbSet<RequestDetail> RequestDetails { get;}
+        DbSet<Schedule> Schedules { get; }
+        DbSet<Status> Statuses { get; }
+        DbSet<Storer> Storers { get;  }
+        DbSet<Student> Students { get; }
+        DbSet<Subject> Subjects { get; }
+        DbSet<Teach> Teaches { get;}
+    }
+}
